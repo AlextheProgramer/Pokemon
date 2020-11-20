76c616c8b79a57183bd029ea669d86d4d2a1d788
Alex <63260259+AlextheProgramer@users.noreply.github.com>
2020-11-20 16:27:03 -0500

    Add files via upload

---
+++ scanboidude.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.Scanner;
+
+public class scanboidude {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Pokemon 1: ");
+		String pok1 = scan.nextLine();
+		System.out.println(pok1);
+		
+		System.out.println("Enter Pokemon 2: ");
+		String pok2 = scan.nextLine();
+		System.out.println(pok2);
+		
+		System.out.println("Enter Pokemon 3: ");
+		String pok3 = scan.nextLine();
+		System.out.println(pok3);
+		
+		System.out.println("Enter Pokemon 4: ");
+		String pok4 = scan.nextLine();
+		System.out.println(pok4);
+		
+		System.out.println("Enter Pokemon 5: ");
+		String pok5 = scan.nextLine();
+		System.out.println(pok5);
+		
+
+	}
+
+}
